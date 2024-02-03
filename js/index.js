0c14c201d2f86cad65203cb5dc2dc33f5631db29
Atulya Kumar <atulyakumar97@gmail.com>
2024-02-03 11:04:54 -0800

    major changes

--- js/index.js
+++ js/index.js
@@ -3,28 +3,28 @@ function displaySection(link,section) {
     // link and the section to show
     let lnk = document.querySelector(`#${link}`);
     let sec = document.querySelector(`#${section}`);
-    
+
     // when you click on one of the links
     lnk.onclick = (event) => {
-        
+
         // mobile js
         const mq = window.matchMedia("(max-width: 500px)");
         if(mq.matches) {
-            // show the go-back button when a link is clicked in responsive mode . 
+            // show the go-back button when a link is clicked in responsive mode .
             let goBackButton = document.querySelector("#go-back-button");
             goBackButton.style.display = "block";
 
-            // when the go-back button is clicked , it disappears with the main and the menu should reapear .
+            // when the go-back button is clicked , it disappears with the main and the menu should reappear .
             goBackButton.addEventListener("click",() => {
                 // hide the mainSection
-                sec.style.display = "none";       
+                sec.style.display = "none";
                 let menu = document.querySelector("#menu");
                 menu.style.display = "flex";
                 goBackButton.style.display = "none";
                 lnk.classList.remove("active");
-         
+
             });
-            lnk.parentElement.style.display = "none"; 
+            lnk.parentElement.style.display = "none";
             if(section == "skills-section") {
                 let div = document.querySelector(`#${section} > article`);
                 div.style.display = "flex";
@@ -33,7 +33,7 @@ function displaySection(link,section) {
                 for (se of subElement){
                     se.style.fontFamily = "roboto mono";
                 }
-                
+
             } else if (section == "projects-section") {
                 let s = document.querySelector("#projects-wrapper");
 
@@ -51,7 +51,7 @@ function displaySection(link,section) {
             sec.style.animationName = "section-mobile";
             sec.style.animationDuration = "1.2s";
         } else {
-            
+
             // remove the section from sight
             for(elem of sec.parentElement.children){
                 elem.style.display = "none";
@@ -65,13 +65,13 @@ function displaySection(link,section) {
             sec.style.animationName = "section";
             sec.style.animationDuration = "0.6s";
             sec.style.animationFillMode = "forwards";
-            
+
 
             let menu = document.querySelector("#menu");
             menu.style.marginTop = "0px";
             menu.style.display = "flex";
             menu.style.position = "fixed";
-            menu.style.top = "1vw"; 
+            menu.style.top = "1vw";
             menu.style.transition = "All 1s";
             menu.style.marginTop="0px";
             menu.style.right= "15vw";
@@ -79,71 +79,76 @@ function displaySection(link,section) {
             event.preventDefault() ;
             lnk.classList.add("active");
         }
-        
+
         let jiran = lnk.parentElement.children;
         for(j of jiran){
             j.classList.remove("active");
         }
 
-            event.preventDefault() ; 
+            event.preventDefault() ;
             lnk.classList.add("active");
 
     };
 }
 
-// dark and light mode 
+// dark and light mode
 document.documentElement.style.setProperty('--background-color', '#fafafa');
 function switchDarkLightMode() {
     let icon = document.querySelector("#switch-container img");
-   
-    icon.onclick = () => {
-    let darkColor = "#111216"; 
-    let darkTextColor = "#999";
-    let darkTextTitleColor = "#eee";
-    let darkTextHoverColor = "#ddd";
-    let darkAccentColor = "#222";
-    icon.onmouseover = () => {
-        console.log("James");
-        icon.setAttribute("src","images/moon-full.svg");
-    }
-    icon.onmouseout = () => {
-        icon.setAttribute("src","images/moon.svg");
-    }
-    let color = getComputedStyle(document.documentElement).getPropertyValue("--background-color");
-    if(color == "#fafafa"){
-        icon.setAttribute("src","images/sun.svg");
-        document.documentElement.style.setProperty('--background-color',darkColor);
-        document.documentElement.style.setProperty('--text-color',darkTextColor);
-        document.documentElement.style.setProperty('--text-title-color',darkTextTitleColor);
-        document.documentElement.style.setProperty('--text-hover-color',darkTextHoverColor);
-        document.documentElement.style.setProperty('--light-accent-color',darkAccentColor);
+
+    function setDarkMode() {
+        let darkColor = "#1F2530"; // Adjusted to a slightly lighter shade
+        let darkTextColor = "#999";
+        let darkTextTitleColor = "#eee";
+        let darkTextHoverColor = "#ddd";
+        let darkAccentColor = "#222";
+
+        document.documentElement.style.setProperty('--background-color', darkColor);
+        document.documentElement.style.setProperty('--text-color', darkTextColor);
+        document.documentElement.style.setProperty('--text-title-color', darkTextTitleColor);
+        document.documentElement.style.setProperty('--text-hover-color', darkTextHoverColor);
+        document.documentElement.style.setProperty('--light-accent-color', darkAccentColor);
+
+        icon.setAttribute("src", "images/sun-filled.svg");
         icon.onmouseover = () => {
-                icon.setAttribute("src","images/sun-filled.svg");
+            icon.setAttribute("src", "images/sun-filled.svg");
         }
         icon.onmouseout = () => {
-                icon.setAttribute("src","images/sun.svg");
+            icon.setAttribute("src", "images/sun.svg");
         }
-    
-    } else {
-        icon.setAttribute("src","images/moon.svg");
+
+        document.documentElement.classList.add('dark-mode'); // Add dark-mode class
+    }
+
+    function setLightMode() {
         document.documentElement.style.setProperty('--background-color', '#fafafa');
         document.documentElement.style.setProperty('--text-color', '#111');
-        document.documentElement.style.setProperty('--text-hover-color',"#000");
-        document.documentElement.style.setProperty('--text-title-color',"#111");
-        document.documentElement.style.setProperty('--light-accent-color',"#ccc");
+        document.documentElement.style.setProperty('--text-hover-color', "#000");
+        document.documentElement.style.setProperty('--text-title-color', "#111");
+        document.documentElement.style.setProperty('--light-accent-color', "#ccc");
 
+        icon.setAttribute("src", "images/moon.svg");
         icon.onmouseover = () => {
-            console.log("James");
-            icon.setAttribute("src","images/moon-full.svg");
+            icon.setAttribute("src", "images/moon-full.svg");
         }
         icon.onmouseout = () => {
-            icon.setAttribute("src","images/moon.svg");
+            icon.setAttribute("src", "images/moon.svg");
+        }
+
+        document.documentElement.classList.remove('dark-mode'); // Remove dark-mode class
+    }
+
+    setDarkMode(); // Set dark mode by default
+
+    icon.onclick = () => {
+        // Toggle between dark and light mode when the icon is clicked
+        if (getComputedStyle(document.documentElement).getPropertyValue("--background-color") == "#fafafa") {
+            setDarkMode();
+        } else {
+            setLightMode();
         }
     }
-    }
 }
-  
-
 
 switchDarkLightMode();
 displaySection("about-me","about-me-section");
